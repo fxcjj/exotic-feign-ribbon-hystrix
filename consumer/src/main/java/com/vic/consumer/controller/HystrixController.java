1be11c595fae7abec59ddd6008420da51b01606f
罗利华 <luolihua@beadwallet.com>
2021-01-07 15:11:28 +0800

    test get cur host ip

--- consumer/src/main/java/com/vic/consumer/controller/HystrixController.java
+++ consumer/src/main/java/com/vic/consumer/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.vic.consumer.controller;
 
 import com.vic.commserver.feign.producer.ProducerClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +19,12 @@ public class HystrixController {
     @Autowired
     ProducerClient producerClient;
 
+    @Value("${spring.cloud.client.ip-address}")
+    String ip;
+
     @GetMapping("t1")
     public String t1(@RequestParam("id") Integer id) {
-        return producerClient.hystrixTest1(id);
+        return producerClient.hystrixTest1(id) + "ip: " + ip;
     }
 
 
