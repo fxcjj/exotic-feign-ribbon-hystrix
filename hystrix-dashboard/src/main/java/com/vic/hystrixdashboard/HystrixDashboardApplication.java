04a3fe0035983d51312b9b5e91d25f57276ca98d
罗利华 <luolihua@beadwallet.com>
2021-01-12 14:46:31 +0800

    turbine-service

---
+++ hystrix-dashboard/src/main/java/com/vic/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.vic.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ *
+ * @author victor
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
